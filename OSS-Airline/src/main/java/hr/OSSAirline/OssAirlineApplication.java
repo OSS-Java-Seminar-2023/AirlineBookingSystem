194579d3d972eaa065b6fcdfe7ab19151206c5a6
v-matej <matej.vaukan@gmail.com>
2023-11-06 16:08:32 +0100

    Adding project

---
+++ OSS-Airline/src/main/java/hr/OSSAirline/OssAirlineApplication.java
@@ -0,0 +1,14 @@
+package hr.OSSAirline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.flywaydb.core.Flyway;
+
+@SpringBootApplication
+public class OssAirlineApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(OssAirlineApplication.class, args);
+	}
+}
---
+++ OSS-Airline/src/test/java/hr/OSSAirline/OssAirlineApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.OSSAirline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OssAirlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
