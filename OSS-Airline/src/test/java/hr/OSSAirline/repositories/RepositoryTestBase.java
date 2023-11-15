0148bf1a7defdb48e72e03656baac69e24c7ad2a
Antonio Bečić <becicantonio27@gmail.com>
2023-11-15 17:44:11 +0100

    Test setup

---
+++ OSS-Airline/src/test/java/hr/OSSAirline/repositories/RepositoryTestBase.java
@@ -0,0 +1,10 @@
+package hr.OSSAirline.repositories;
+
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
+
+public class RepositoryTestBase {
+    @BeforeTestMethod
+    protected void beforeMethod(){
+
+    }
+}
