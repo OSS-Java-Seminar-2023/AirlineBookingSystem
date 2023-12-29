cadeff62a9dbf19cf8fe00b8e01196db794a7231
v-matej <matej.vaukan@gmail.com>
2023-12-29 11:30:39 +0100

    Security added and link protection

--- OSS-Airline/src/main/java/hr/OSSAirline/controllers/AirportController.java
+++ OSS-Airline/src/main/java/hr/OSSAirline/controllers/AirportController.java
@@ -2,6 +2,7 @@ package hr.OSSAirline.controllers;
 
 import hr.OSSAirline.dto.AirportDto;
 import hr.OSSAirline.services.AirportService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,9 +16,10 @@ public class AirportController {
 
     private final AirportService airportService;
     @GetMapping("/")
-    public String home(Model model) {
+    public String home(Model model, HttpSession session) {
         List<AirportDto> airports = airportService.getAllAirports();
         model.addAttribute("airports", airports);
+        model.addAttribute("httpSession",session);
         return "index";
     }
 }
