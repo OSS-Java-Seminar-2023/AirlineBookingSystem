7acda5948aeef0817116542ad840aaa80a7ea4ab
v-matej <matej.vaukan@gmail.com>
2023-12-31 21:27:36 +0100

    Reservation

--- OSS-Airline/src/main/java/hr/OSSAirline/services/TicketPurchaseService.java
+++ OSS-Airline/src/main/java/hr/OSSAirline/services/TicketPurchaseService.java
@@ -2,11 +2,16 @@ package hr.OSSAirline.services;
 
 import hr.OSSAirline.controllers.TicketPurchaseController;
 import hr.OSSAirline.mappers.PurchaseMapper;
+import hr.OSSAirline.models.Flight;
+import hr.OSSAirline.models.Passenger;
+import hr.OSSAirline.models.Seat;
+import hr.OSSAirline.models.Ticket;
 import hr.OSSAirline.repositories.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Service
 @RequiredArgsConstructor
@@ -21,7 +26,7 @@ public class TicketPurchaseService {
 
 
     public void makePurchase(TicketPurchaseController.TicketForm ticketForm) {
-        var tickets = ticketForm.getTickets();
+//        var tickets = ticketForm.getTickets();
         var flights = ticketForm.getFlight().stream().map(flightId -> flightRepository.findById(flightId)).collect(Collectors.toList());
         var passengers = ticketForm.getPassenger().stream().map(passengerId -> passengerRepository.findById(passengerId)).collect(Collectors.toList());
         var seats = ticketForm.getSeat().stream().map(seatId -> seatRepository.findById(seatId)).collect(Collectors.toList());
