c001e3b04c7d76f9aedf8d83baa9b32da375c467
Antonio Bečić <becicantonio27@gmail.com>
2024-02-05 14:11:15 +0100

    misc

--- OSS-Airline/src/main/java/hr/OSSAirline/services/TicketReservationService.java
+++ OSS-Airline/src/main/java/hr/OSSAirline/services/TicketReservationService.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,7 +21,6 @@ public class TicketReservationService {
     private final UserRepository userRepository;
 
 
-
     public void makeReservation(TicketForm ticketForm, String userName) {
         var user = userRepository.findUserByUsername(userName);
         var flights = ticketForm.getFlight().stream().map(flightRepository::findById).toList();
@@ -27,21 +28,27 @@ public class TicketReservationService {
         var seats = ticketForm.getSeat().stream().map(seatRepository::findById).toList();
         var prices = ticketForm.getPrice().stream().toList();
         var ticket_list = new ArrayList<Ticket>();
-        for (int i = 0; i < passengers.size(); i++) {
-            var ticket = new Ticket();
-            ticket.setFlight(flights.get(i).orElseThrow());
-            ticket.setPassenger(passengers.get(i).orElseThrow());
-            ticket.setSeat(seats.get(i).orElseThrow());
-            ticket.setTicketPrice(prices.get(i));
-            ticket_list.add(ticket);
-        }
+        generateTickets(passengers, flights, seats, prices, ticket_list);
         var reservation = new Reservation();
-        reservation.setUser(user.get());
+        reservation.setUser(user.orElseThrow());
         reservation.setTickets(ticket_list);
         reservationRepository.save(reservation);
         ticket_list.forEach(ticket -> {
                     ticket.setReservation(reservation);
                     ticketRepository.updateReservationField(ticket.getId(),reservation);
                 });
+    }
+
+    private static void generateTickets(List<Optional<Passenger>> passengers, List<Optional<Flight>> flights, List<Optional<Seat>> seats, List<Float> prices, ArrayList<Ticket> ticket_list) {
+        int index = 0;
+        for (var passenger : passengers) {
+            var ticket = new Ticket();
+            ticket.setFlight(flights.get(index).orElseThrow());
+            ticket.setPassenger(passenger.orElseThrow());
+            ticket.setSeat(seats.get(index).orElseThrow());
+            ticket.setTicketPrice(prices.get(index));
+            ticket_list.add(ticket);
+            index++;
+        }
     }
 }
--- OSS-Airline/src/test/java/hr/OSSAirline/repositories/testInstances/AirplaneIntegrationTest.java
+++ OSS-Airline/src/test/java/hr/OSSAirline/repositories/testInstances/AirplaneIntegrationTest.java
@@ -4,6 +4,7 @@ import hr.OSSAirline.models.Airplane;
 import hr.OSSAirline.repositories.AirplaneRepository;
 import hr.OSSAirline.repositories.RepositoryTestBase;
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,6 +28,98 @@ public class AirplaneIntegrationTest extends RepositoryTestBase {
         airplaneRepository.save(testAirplane);
 
         var out = airplaneRepository.getAirplaneByRegistration(testAirplane.getRegistration());
-        Assert.assertEquals("Test Passed", out.getRegistration(), testAirplane.getRegistration());
+        Assertions.assertEquals(out.getRegistration(), testAirplane.getRegistration(), "Test Passed");
+    }
+
+    @Test
+    public void airplaneSaveAndGetByRegistration2(){
+        var testAirplane = new Airplane();
+        testAirplane.setRegistration("12344325");
+        testAirplane.setModel("B2732");
+        testAirplane.setBusinessSeats(1);
+        testAirplane.setFirstSeats(1);
+        testAirplane.setEconomySeats(1);
+        airplaneRepository.save(testAirplane);
+
+        var out = airplaneRepository.getAirplaneByRegistration(testAirplane.getRegistration());
+        Assertions.assertEquals(out.getRegistration(), testAirplane.getRegistration(), "Test Passed");
+    }
+
+    @Test
+    public void airplaneSaveAndGetByRegistration3(){
+        var testAirplane = new Airplane();
+        testAirplane.setRegistration("4325");
+        testAirplane.setModel("B");
+        testAirplane.setBusinessSeats(1);
+        testAirplane.setFirstSeats(1);
+        testAirplane.setEconomySeats(1);
+        airplaneRepository.save(testAirplane);
+
+        var out = airplaneRepository.getAirplaneByRegistration(testAirplane.getRegistration());
+        Assertions.assertEquals(out.getRegistration(), testAirplane.getRegistration(), "Test Passed");
+    }
+
+    @Test
+    public void airplaneSaveAndGetNullByRegistration(){
+        var testAirplane = new Airplane();
+        testAirplane.setRegistration("12878345");
+        testAirplane.setModel("B272");
+        testAirplane.setBusinessSeats(30);
+        testAirplane.setFirstSeats(20);
+        testAirplane.setEconomySeats(40);
+        airplaneRepository.save(testAirplane);
+
+        var wrong_reg = "wrong";
+
+        var out = airplaneRepository.getAirplaneByRegistration(wrong_reg);
+        Assertions.assertNull(out, "Test Passed");
+    }
+
+    @Test
+    public void airplaneSaveAndGetNullByRegistration2(){
+        var testAirplane = new Airplane();
+        testAirplane.setRegistration("12323445");
+        testAirplane.setModel("B272");
+        testAirplane.setBusinessSeats(30);
+        testAirplane.setFirstSeats(20);
+        testAirplane.setEconomySeats(40);
+        airplaneRepository.save(testAirplane);
+
+        var wrong_reg = "wrong";
+
+        var out = airplaneRepository.getAirplaneByRegistration(wrong_reg);
+        Assertions.assertNull(out, "Test Passed");
+    }
+
+    @Test
+    public void airplaneSaveAndGetNullByRegistration3(){
+        var testAirplane = new Airplane();
+        testAirplane.setRegistration("123565645");
+        testAirplane.setModel("B272");
+        testAirplane.setBusinessSeats(30);
+        testAirplane.setFirstSeats(20);
+        testAirplane.setEconomySeats(40);
+        airplaneRepository.save(testAirplane);
+
+        var wrong_reg = "wrong";
+
+        var out = airplaneRepository.getAirplaneByRegistration(wrong_reg);
+        Assertions.assertNull(out, "Test Passed");
+    }
+
+    @Test
+    public void airplaneSaveAndGetNullByRegistration4(){
+        var testAirplane = new Airplane();
+        testAirplane.setRegistration("12767686345");
+        testAirplane.setModel("B272");
+        testAirplane.setBusinessSeats(30);
+        testAirplane.setFirstSeats(20);
+        testAirplane.setEconomySeats(40);
+        airplaneRepository.save(testAirplane);
+
+        var wrong_reg = "wrong";
+
+        var out = airplaneRepository.getAirplaneByRegistration(wrong_reg);
+        Assertions.assertNull(out, "Test Passed");
     }
 }
